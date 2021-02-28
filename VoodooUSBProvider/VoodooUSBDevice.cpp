5c79c4548a464f14fb97a840d9dce140ee33602b
CharlieJiangXXX <45895922+CharlieJiangXXX@users.noreply.github.com>
2021-02-28 09:41:08 +0800

    Delete VoodooUSBDevice.cpp

--- VoodooUSBProvider/VoodooUSBDevice.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  VoodooUSBDevice.cpp
-//  VoodooUSBProvider8
-//
-//  Created by Charlie Jiang on 2/26/21.
-//  Copyright © 2021 Charlie Jiang. All rights reserved.
-//
-
-#include "VoodooUSBDevice.hpp"
