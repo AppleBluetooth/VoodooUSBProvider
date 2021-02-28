7550d662f39b6e8952457670e8f488e4ca83ca65
CharlieJiangXXX <45895922+CharlieJiangXXX@users.noreply.github.com>
2021-02-28 09:41:27 +0800

    Delete VoodooUSBHostDevice.cpp

--- VoodooUSBProvider/VoodooUSBHostDevice.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  VoodooUSBHostDevice.cpp
-//  VoodooUSBProvider
-//
-//  Created by Charlie Jiang on 2/26/21.
-//  Copyright © 2021 Charlie Jiang. All rights reserved.
-//
-
-#include <stdio.h>
